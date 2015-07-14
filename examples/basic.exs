95ee2374a4a2087289aced7f78e6a448edd5e6da
Josh Adams <josh@isotope11.com>
2015-07-14 17:54:17 -0500

    Drawing a few sample data points

--- examples/basic.exs
+++ examples/basic.exs
@@ -1,1 +1,7 @@
-AudioRenderer.Window.start([])
+AudioRenderer.Window.start([
+  {0, 100},
+  {1, 99},
+  {2, 50},
+  {3, 20},
+  {4, 100}
+])
--- lib/audio_visualizer/renderer.ex
+++ lib/audio_visualizer/renderer.ex
@@ -1,16 +1,21 @@
 defmodule AudioVisualizer.Renderer do
+  @center_y 100
+
   def render(canvas, data) do
     draw_lines(canvas, data)
   end
 
-  def draw_lines(canvas, _data) do
-    draw_line(canvas)
+  def draw_lines(canvas, [head|rest]) do
+    draw_line(canvas, head)
+    draw_lines(canvas, rest)
   end
+  def draw_lines(canvas, []), do: :ok
 
-  def draw_line(canvas) do
+  def draw_line(canvas, {position, amplitude}) do
+    IO.puts amplitude
     pen = :wxPen.new({255, 0, 0, 255})
     :wxGraphicsContext.setPen(canvas, pen)
-    :wxGraphicsContext.drawLines(canvas, [{100, 100}, {200, 200}])
+    :wxGraphicsContext.drawLines(canvas, [{0 + position, @center_y}, {0 + position, amplitude}])
     :ok
   end
 end
