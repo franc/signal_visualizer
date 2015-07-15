afe539e480296fa9bb3f766e69b3cd4e40cd578c
Josh Adams <josh@isotope11.com>
2015-07-15 17:15:19 -0500

    The actual output of episode 177

---
+++ examples/get_audio.exs
@@ -0,0 +1,25 @@
+{:ok, data_agent} = Agent.start_link(fn -> [] end)
+
+defmodule AudioReceiver do
+  def start(data_agent) do
+    Port.open({:spawn, "parec --rate=16000 --format=u8"}, [])
+    loop(data_agent)
+  end
+
+  def loop(data_agent) do
+    receive do
+      {_, {:data, data}} ->
+        Agent.update(data_agent, fn(existing_data) ->
+          data ++ existing_data
+        end)
+      other -> IO.inspect "got other: #{inspect other}"
+    end
+    loop(data_agent)
+  end
+end
+
+spawn(fn() ->
+  AudioReceiver.start(data_agent)
+end)
+
+AudioVisualizer.Window.start(data_agent)
--- lib/audio_visualizer/renderer.ex
+++ lib/audio_visualizer/renderer.ex
@@ -1,16 +1,43 @@
 defmodule AudioVisualizer.Renderer do
+  @center_y 100
+  @resolution 16
+  @multiplier 3
+
   def render(canvas, data) do
-    draw_lines(canvas, data)
+    draw_lines(canvas, Enum.with_index(data))
   end
 
-  def draw_lines(canvas, _data) do
-    draw_line(canvas)
+  def draw_lines(canvas, data) do
+    sampled_data = sample(data)
+    do_draw_lines(canvas, points(sampled_data))
   end
 
-  def draw_line(canvas) do
+  def sample(data) do
+    sample(data, [])
+  end
+  def sample([{amplitude, position}|rest], acc) do
+    case rem(position, @resolution) do
+      0 ->
+        sample(rest, [{amplitude, position}|acc])
+      _ ->
+        sample(rest, acc)
+    end
+  end
+  def sample([], acc), do: Enum.reverse(acc)
+
+  def points(sampled_data) do
+    Enum.map(sampled_data, fn({amplitude, position}) ->
+      x = 0 + position/@resolution
+      y = -127 + amplitude * @multiplier
+      {x, @center_y + y}
+    end)
+  end
+
+  def do_draw_lines(_canvas, []), do: :ok
+  def do_draw_lines(canvas, points) do
     pen = :wxPen.new({255, 0, 0, 255})
     :wxGraphicsContext.setPen(canvas, pen)
-    :wxGraphicsContext.drawLines(canvas, [{100, 100}, {200, 200}])
+    :wxGraphicsContext.drawLines(canvas, points)
     :ok
   end
 end
